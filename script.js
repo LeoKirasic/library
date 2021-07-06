20d276b4262893059883cc618640028c173fb6ef
LeoKirasic <kirasicleo@gmail.com>
2021-07-06 17:11:42 +0200

    added simple popup form

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary('thehobbit', 'jk rowling', '250', 'no');
 addBookToLibrary('testbook', 'testname', 'testpages', 'yes');
 addBookToLibrary('a', 'b', 'c', 'd');
-
+addBookToLibrary('agasga', 'bgsaga', 'cgsag', 'dgsaga');
 
 const container = document.querySelector('#book-container');
 
@@ -33,3 +33,61 @@ function displayBooks () {
 }
 
 displayBooks();
+
+const addBookBtn = document.querySelector('#add-book');
+const formContent = document.querySelector('#form-content');
+addBookBtn.addEventListener(('click'), () => {
+  const form = document.createElement('form');
+  // title input
+
+  const titleHeading = document.createElement('div');
+  titleHeading.classList.add = ('form-heading');
+  titleHeading.textContent = 'TITLE';
+  form.appendChild(titleHeading);
+
+  const titleText = document.createElement('input');
+  titleText.setAttribute('type', 'titleText');
+  form.appendChild(titleText);
+  let lineBreak = document.createElement('br');
+  form.appendChild(lineBreak);
+  formContent.appendChild(form);
+  // author input
+
+  const authorHeading = document.createElement('div');
+  authorHeading.classList.add = ('form-heading');
+  authorHeading.textContent = 'AUTHOR';
+  form.appendChild(authorHeading);
+
+  const authorText = document.createElement('input');
+  titleText.setAttribute('type', 'authorText');
+  form.appendChild(authorText);
+  lineBreak = document.createElement('br');
+  form.appendChild(lineBreak);
+  formContent.appendChild(form);
+
+  // pages input
+  const pagesHeading = document.createElement('div');
+  pagesHeading.classList.add = ('form-heading');
+  pagesHeading.textContent = 'PAGES';
+  form.appendChild(pagesHeading);
+
+  const pagesText = document.createElement('input');
+  pagesText.setAttribute('type', 'pagesText');
+  form.appendChild(pagesText);
+  lineBreak = document.createElement('br');
+  form.appendChild(lineBreak);
+  formContent.appendChild(form);
+
+  // read input
+  const readHeading = document.createElement('div');
+  readHeading.classList.add = ('form-heading');
+  readHeading.textContent = 'READ';
+  form.appendChild(readHeading);
+
+  const readText = document.createElement('input');
+  readText.setAttribute('type', 'checkbox');
+  form.appendChild(readText);
+  lineBreak = document.createElement('br');
+  form.appendChild(lineBreak);
+  formContent.appendChild(form);
+});
